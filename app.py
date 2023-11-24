fce5e2654f25513e214020e25d0e18219eb14157
alelele5946 <alejapat2001@gmail.com>
2023-11-24 12:07:51 +0000

    Modified so code updates the file sent from SB and the updates the same row calling Supabase through the API

--- app.py
+++ app.py
@@ -1,62 +1,72 @@
-from PyPDF2.generic import RectangleObject
-from flask import Flask, request, send_from_directory, jsonify
+from flask import Flask, request, jsonify
 from PyPDF2 import PdfReader, PdfWriter
-import os
+from PyPDF2.generic import RectangleObject
 import requests
 import io
+import os
+
+# Asegúrate de que las variables de entorno estén configuradas en Heroku
+SUPABASE_API_KEY = os.environ.get('SUPABASE_API_KEY')
+SUPABASE_BEARER_TOKEN = os.environ.get('SUPABASE_BEARER_TOKEN')
+
 
 app = Flask(__name__)
 
 
-@app.route('/test-crop', methods=['POST'])  # Aceptar solicitudes POST
+@app.route('/test-crop', methods=['POST'])
 def test_crop():
-    # Obtener el JSON del cuerpo de la solicitud
     data = request.get_json()
-
-    # Obtener la URL del PDF del JSON
     pdf_url = data.get('URL_PDF')
+    row_id = data.get('id')
 
-    if not pdf_url:
-        return jsonify({"error": "No se proporcionó la URL del PDF."}), 400
+    if not row_id or not pdf_url:
+        return jsonify({"error": "Falta el 'id' de la fila o la URL del PDF."}), 400
 
     try:
-        # Hacer una solicitud GET para descargar el PDF
+        # Descargar el PDF
         response = requests.get(pdf_url)
-        response.raise_for_status()  # Asegurarse de que la solicitud fue exitosa
+        response.raise_for_status()
 
-        # Leer el PDF desde la respuesta de la solicitud
+        # Leer el PDF desde la respuesta
         reader = PdfReader(io.BytesIO(response.content))
         writer = PdfWriter()
 
-        # Ruta al archivo PDF recortado
-        output_pdf_path = os.path.join('static', 'pdfs', 'test_cropped.pdf')
+        # Recortar cada página y extraer texto
+        extracted_text = ""
+        for page in reader.pages:
+            page.cropbox = RectangleObject((100.3, 0, 612, 792))  # Recortar la página
+            writer.add_page(page)
+            text = page.extract_text()
+            if text:
+                extracted_text += text + "\n"
 
-        # Ruta al archivo de texto donde se guardará el texto extraído
-        output_txt_path = os.path.join('static', 'txt', 'test_extracted.txt')
+        # Ahora que tienes el texto, puedes enviarlo a Supabase
+        supabase_url = 'https://mgfdhvmvuthxvfyriacu.supabase.co/rest/v1/Documentos'
+        headers = {
+            "apikey": SUPABASE_API_KEY,
+            "Authorization": SUPABASE_BEARER_TOKEN,
+            "Content-Type": "application/json",
+            "Prefer": "return=minimal"
+        }
+        # Supongamos que quieres actualizar la columna 'document_text' del documento que cumple cierta condición
+        data_to_patch = {
+            "Doc_TXT": extracted_text  # Asegúrate de que esta columna exista en tu tabla de Supabase
+        }
+        # Aquí necesitas especificar cómo identificar el registro a actualizar, por ejemplo:
+        params = {
+            "id": f"eq.{row_id}"  # Reemplaza con la condición apropiada
+        }
+        # Realiza la petición PATCH para actualizar el registro en Supabase
+        response = requests.patch(supabase_url, headers=headers, json=data_to_patch, params=params)
+        response.raise_for_status()
 
-        # Abre el archivo de texto en modo de escritura
-        with open(output_txt_path, 'w') as txt_outstream:
-            # Recortar cada página y extraer texto
-            for page in reader.pages:
-                text = page.extract_text()
-                if text:
-                    txt_outstream.write(text + "\n")  # Escribe el texto en el archivo de texto
-                page.cropbox = RectangleObject((0, 0, 500, 792))
-                writer.add_page(page)
 
-        # Escribir el archivo PDF recortado
-        with open(output_pdf_path, 'wb') as outstream:
-            writer.write(outstream)
+        return jsonify({"message": "PDF procesado y texto actualizado en Supabase con éxito."})
 
     except requests.HTTPError as e:
-        print(f"Ocurrió un error al descargar el PDF: {e}")
-        return jsonify({"error": f"Error al descargar el PDF: {e}"}), 500
+        return jsonify({"error": f"Error al procesar la solicitud: {e}"}), 500
     except Exception as e:
-        print(f"Ocurrió un error al procesar el PDF: {e}")
         return jsonify({"error": f"Error al procesar el archivo PDF: {e}"}), 500
-
-    # Devuelve el archivo PDF recortado
-    return send_from_directory('static/pdfs', 'test_cropped.pdf', as_attachment=True)
 
 
 if __name__ == '__main__':
